8391ecd6b3e865a3153f2943146ec7f0e85c428c
Nagarjun Sanji <nsanji@opentext.com>
2024-07-08 13:56:35 +0530

    added error message

--- resources/debricked-cli/install-debricked.bat
+++ resources/debricked-cli/install-debricked.bat
@@ -26,37 +26,66 @@ set "installPath=C:\Program Files\debricked"
 
 :: Download the file
 echo Downloading Debricked CLI from %downloadUrl%
-powershell -Command "Invoke-WebRequest -Uri '%downloadUrl%' -OutFile '%destinationPath%'"
+powershell -Command "Invoke-WebRequest -Uri '%downloadUrl%' -OutFile '%destinationPath%' -UseBasicParsing; if ($?) { exit 0 } else { exit 1 }"
+if %errorLevel% neq 0 (
+    echo Failed to download Debricked CLI
+    pause
+)
 
 :: Create the extract path if it doesn't exist
-if not exist "%extractPath%" mkdir "%extractPath%"
+if not exist "%extractPath%" (
+    mkdir "%extractPath%" || (
+        echo Failed to create extract path
+        pause
+    )
+)
 
 :: Extract the tar.gz file
 echo Extracting Debricked CLI to %extractPath% ...
-tar -xzf "%destinationPath%" -C "%extractPath%"
+tar -xzf "%destinationPath%" -C "%extractPath%" || (
+    echo Failed to extract Debricked CLI
+    pause
+)
 
 :: Remove the tar.gz file
-del "%destinationPath%"
+del "%destinationPath%" || (
+    echo Failed to delete tar.gz file
+    pause
+)
 
 :: Create the install path if it doesn't exist
-if not exist "%installPath%" mkdir "%installPath%"
+if not exist "%installPath%" (
+    mkdir "%installPath%" || (
+        echo Failed to create install path
+        pause
+    )
+)
 
 :: Copy the extracted debricked.exe to the install path
 echo Installing Debricked CLI to %installPath% ...
-copy "%extractPath%\debricked.exe" "%installPath%\debricked.exe" /Y
+copy "%extractPath%\debricked.exe" "%installPath%\debricked.exe" /Y || (
+    echo Failed to copy Debricked CLI to install path
+    pause
+)
 
 :: Add the install path to the system PATH if not already present
 echo Adding Debricked CLI to system PATH...
 for /f "tokens=2*" %%A in ('reg query "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\Environment" /v Path') do set "currentPath=%%B"
 echo %currentPath% | find /i "%installPath%" > nul
 if errorlevel 1 (
-    setx /M PATH "%currentPath%;%installPath%"
+    setx /M PATH "%currentPath%;%installPath%" || (
+        echo Failed to add Debricked CLI to system PATH
+        pause
+    )
 )
 
 :: Clean up the extraction path
-rmdir /s /q "%extractPath%"
+rmdir /s /q "%extractPath%" || (
+    echo Failed to clean up extraction path
+    pause
+)
 
 echo Debricked(%releaseVersion%) CLI installation completed successfully.
 
 :: Exit the terminal
-exit
+pause
