5aeb1b9f714ddf00fdc9700735956f41b5336457
Nagarjun Sanji <nsanji@opentext.com>
2024-07-12 11:26:19 +0530

    test case fixes

--- src/test/helpers/authHelper.test.ts
+++ src/test/helpers/authHelper.test.ts
@@ -1,23 +1,79 @@
-import { sinon, expect } from "../setup";
-import { AuthHelper } from "../../helpers";
-import { Messages, Organization } from "../../constants";
+import { Messages, Organization } from "../../constants/index";
+import { expect, sinon } from "../setup";
+import proxyquire from "proxyquire";
+import path from "path";
+import fs from "fs";
 
-describe("Auth Helper: Test Suite", () => {
-    let sandbox: sinon.SinonSandbox;
-    const fakeWorkspace = "/fake/workspace";
+describe("AuthHelper", () => {
+    let AuthHelper: any;
+    let fsStub: any;
+    let vscodeStub: any;
+    let originalWorkspace: string;
+    let mockWorkspacePath: string;
 
     beforeEach(() => {
-        sandbox = sinon.createSandbox();
-        sandbox.stub(Organization, "workspace").value(fakeWorkspace);
+        // Create a mock workspace in the current directory
+        mockWorkspacePath = path.join(__dirname, "../../", Organization.debrickedFolder);
+        console.log(mockWorkspacePath);
+        if (!fs.existsSync(mockWorkspacePath)) {
+            fs.mkdirSync(mockWorkspacePath);
+        }
+
+        fsStub = {
+            existsSync: sinon.stub(),
+            mkdirSync: sinon.stub(),
+            readFileSync: sinon.stub(),
+            writeFileSync: sinon.stub(),
+        };
+
+        vscodeStub = {
+            window: {
+                showInputBox: sinon.stub(),
+            },
+            workspace: {
+                workspaceFolders: [
+                    {
+                        uri: {
+                            fsPath: mockWorkspacePath,
+                        },
+                    },
+                ],
+            },
+        };
+
+        originalWorkspace = Organization.workspace;
+        Object.defineProperty(Organization, "workspace", {
+            value: mockWorkspacePath,
+            writable: true,
+        });
+
+        AuthHelper = proxyquire("../../helpers", {
+            fs: fsStub,
+            vscode: vscodeStub,
+            path: path,
+        }).AuthHelper;
     });
 
     afterEach(() => {
-        sandbox.restore();
+        sinon.restore();
+        Object.defineProperty(Organization, "workspace", {
+            value: originalWorkspace,
+            writable: false,
+        });
+        // Clean up the mock workspace
+        if (fs.existsSync(mockWorkspacePath)) {
+            fs.rmdirSync(mockWorkspacePath, { recursive: true });
+        }
     });
 
-    it("should throw an error if workspace is not found", async () => {
-        sandbox.stub(Organization, "workspace").value(undefined);
+    describe("getAccessToken", () => {
+        it("should throw an error if workspace is not defined", async () => {
+            Object.defineProperty(Organization, "workspace", {
+                value: "",
+                writable: true,
+            });
 
-        await expect(AuthHelper.getAccessToken()).to.eventually.be.rejectedWith(Messages.WS_NOT_FOUND);
+            await expect(AuthHelper.getAccessToken()).to.be.rejectedWith(Messages.WS_NOT_FOUND);
+        });
     });
 });
