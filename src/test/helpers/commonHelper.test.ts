3eff3e8688834eadf7f0bdb2f1a93beac6b2c6a8
Nagarjun Sanji <nsanji@opentext.com>
2024-07-12 08:55:50 +0530

    test case fix

--- src/test/helpers/commonHelper.test.ts
+++ src/test/helpers/commonHelper.test.ts
@@ -1,10 +1,87 @@
+import * as vscode from "vscode";
+import * as path from "path";
+import proxyquire from "proxyquire";
 import { Common } from "../../helpers";
+import * as crypto from "crypto";
 import { expect, sinon } from "../setup";
 
-describe("Common Helper : Test Suite", () => {
-    it("Check User Id", () => {
-        const checkUserId = sinon.spy(Common, "checkUserId");
-        checkUserId();
-        expect(checkUserId).to.be.a("function");
+describe("Common", () => {
+    let showInputBoxStub: sinon.SinonStub;
+    let fsExistsSyncStub: sinon.SinonStub;
+    let fsReadFileSyncStub: sinon.SinonStub;
+    let fsWriteFileSyncStub: sinon.SinonStub;
+    let fsMkdirSyncStub: sinon.SinonStub;
+
+    const mockPath = path.join("mockWorkspace", ".debricked", "debricked_data.json");
+
+    before(() => {
+        showInputBoxStub = sinon.stub(vscode.window, "showInputBox");
+
+        fsExistsSyncStub = sinon.stub();
+        fsReadFileSyncStub = sinon.stub();
+        fsWriteFileSyncStub = sinon.stub();
+        fsMkdirSyncStub = sinon.stub();
+
+        proxyquire("../../helpers", {
+            fs: {
+                existsSync: fsExistsSyncStub,
+                readFileSync: fsReadFileSyncStub,
+                writeFileSync: fsWriteFileSyncStub,
+                mkdirSync: fsMkdirSyncStub,
+            },
+        }).Common;
+    });
+
+    afterEach(() => {
+        sinon.resetHistory();
+    });
+
+    after(() => {
+        showInputBoxStub.restore();
+    });
+
+    it("should get input from the user", async () => {
+        const prompt = "Enter something";
+        showInputBoxStub.resolves("test input");
+
+        const result = await Common.getInput(prompt);
+
+        expect(result).to.equal("test input");
+        expect(showInputBoxStub.calledOnceWith({ prompt })).to.be.true;
+    });
+
+    it("should retrieve data from debricked data file", async () => {
+        const key = "testKey";
+        const value = "testValue";
+        fsExistsSyncStub.withArgs(mockPath).returns(true);
+        fsReadFileSyncStub.withArgs(mockPath, "utf-8").returns(JSON.stringify({ [key]: value }));
+
+        const result = await Common.getFromDebrickedData(key);
+
+        expect(result).to.equal(value);
+    });
+
+    it("should return 'unknown-user' if data not found in debricked data file", async () => {
+        fsExistsSyncStub.withArgs(mockPath).returns(false);
+
+        const result = await Common.getFromDebrickedData("testKey");
+
+        expect(result).to.equal("testValue");
+    });
+
+    it("should generate hash code", () => {
+        const input = "test input";
+        const hashCode = Common.generateHashCode(input);
+
+        expect(hashCode).to.equal(crypto.createHash("sha256").update(input).digest("hex"));
+    });
+
+    it("should not generate new user ID if user ID is known", async () => {
+        fsExistsSyncStub.withArgs(mockPath).returns(true);
+        fsReadFileSyncStub.withArgs(mockPath, "utf-8").returns(JSON.stringify({ user_id: "known-user" }));
+
+        await Common.checkUserId();
+
+        expect(fsWriteFileSyncStub.notCalled).to.be.true;
     });
 });
---
+++ src/test/helpers/terminalHelper.test.ts
@@ -0,0 +1,68 @@
+import * as vscode from "vscode";
+import { Terminal } from "../../helpers";
+import { AuthHelper, Logger } from "../../helpers";
+import { Messages, Organization } from "../../constants";
+import { expect, sinon } from "../setup";
+
+describe("Terminal", () => {
+    let createTerminalStub: sinon.SinonStub;
+    let sendTextStub: sinon.SinonStub;
+    let showStub: sinon.SinonStub;
+    let getAccessTokenStub: sinon.SinonStub;
+    let logMessageStub: sinon.SinonStub;
+
+    const mockTerminal: vscode.Terminal = {
+        sendText: () => {},
+        show: () => {},
+    } as any;
+
+    before(() => {
+        createTerminalStub = sinon.stub(vscode.window, "createTerminal").returns(mockTerminal);
+        sendTextStub = sinon.stub(mockTerminal, "sendText");
+        showStub = sinon.stub(mockTerminal, "show");
+        getAccessTokenStub = sinon.stub(AuthHelper, "getAccessToken").resolves("testAccessToken");
+        logMessageStub = sinon.stub(Logger, "logMessage");
+    });
+
+    afterEach(() => {
+        sinon.resetHistory();
+    });
+
+    after(() => {
+        createTerminalStub.restore();
+        sendTextStub.restore();
+        showStub.restore();
+        getAccessTokenStub.restore();
+        logMessageStub.restore();
+    });
+
+    it("should create and use terminal without access token", async () => {
+        const description = "Test Terminal";
+        const cmdParams = ["-v"];
+        const accessTokenRequired = false;
+
+        await Terminal.createAndUseTerminal(description, cmdParams, accessTokenRequired);
+
+        expect(createTerminalStub.calledOnceWith(description)).to.be.true;
+        expect(sendTextStub.calledOnceWith(`${Organization.debricked_cli} ${cmdParams.join(" ")}`)).to.be.true;
+        expect(showStub.calledOnce).to.be.true;
+        expect(
+            logMessageStub.calledOnceWith(
+                `${Messages.CMD_EXEC_WITHOUT_ACCESS_TOKEN}: ${Organization.debricked_cli} ${cmdParams.join(" ")}`,
+            ),
+        ).to.be.true;
+    });
+
+    it("should create and use terminal with access token", async () => {
+        const description = "Test Terminal";
+        const cmdParams = ["-t", "access_token"];
+        const accessTokenRequired = true;
+
+        await Terminal.createAndUseTerminal(description, cmdParams, accessTokenRequired);
+
+        expect(createTerminalStub.calledOnceWith(description)).to.be.true;
+        expect(sendTextStub.calledOnceWith(`${Organization.debricked_cli} ${cmdParams.join(" ")}`)).to.be.true;
+        expect(showStub.calledOnce).to.be.true;
+        expect(logMessageStub.calledOnceWith(Messages.CMD_EXEC_WITH_ACCESS_TOKEN)).to.be.true;
+    });
+});
