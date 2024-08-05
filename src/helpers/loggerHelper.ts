68fa3383dfc2022129e574dfa7f9c684fef8320c
Nagarjun Sanji <nsanji@opentext.com>
2024-08-05 11:35:32 +0530

    reverted logger changes

--- src/helpers/loggerHelper.ts
+++ src/helpers/loggerHelper.ts
@@ -1,6 +1,7 @@
 import * as fs from "fs";
 import * as path from "path";
 import { Organization, MessageStatus } from "../constants/index";
+import { GlobalState } from "./globalState";
 import * as vscode from "vscode";
 import { GlobalStore } from "./globalStore";
 
@@ -25,9 +26,11 @@ export class Logger {
 
     private static async writeLog(message: string) {
         const timestamp = new Date().toISOString();
-        const userId = await GlobalStore.getInstance()
-            .getGlobalStateInstance()
-            ?.getGlobalData(Organization.debrickedDataKey, "", Organization.userId);
+        const userId = await GlobalState.getInstance().getGlobalData(
+            Organization.debrickedDataKey,
+            "",
+            Organization.userId,
+        );
         const sequenceId = GlobalStore.getInstance().getSequenceID()
             ? `[seq_id:${GlobalStore.getInstance().getSequenceID()}]`
             : "";
