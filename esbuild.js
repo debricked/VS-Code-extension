ad572684c374feb10f8335f4990bc380d168ed6f
Nagarjun Sanji <nagarjun.sanji@gmail.com>
2024-09-19 12:03:59 +0530

    41 sentry integration (#59)
    * added to dev dependency
    
    * sentry config
    
    * update
    
    ---------
    
    Co-authored-by: Nagarjun Sanji <nsanji@opentext.com>

--- esbuild.js
+++ esbuild.js
@@ -1,4 +1,5 @@
 const esbuild = require("esbuild");
+const { sentryEsbuildPlugin } = require("@sentry/esbuild-plugin");
 
 const production = process.argv.includes("--production");
 const watch = process.argv.includes("--watch");
@@ -16,9 +17,7 @@ const esbuildProblemMatcherPlugin = {
         build.onEnd((result) => {
             result.errors.forEach(({ text, location }) => {
                 console.error(`✘ [ERROR] ${text}`);
-                console.error(
-                    `    ${location.file}:${location.line}:${location.column}:`,
-                );
+                console.error(`    ${location.file}:${location.line}:${location.column}:`);
             });
             console.log("[watch] build finished");
         });
@@ -40,6 +39,12 @@ async function main() {
         plugins: [
             /* add to the end of plugins array */
             esbuildProblemMatcherPlugin,
+            sentryEsbuildPlugin({
+                authToken: process.env.SENTRY_AUTH_TOKEN,
+                org: "debricked",
+                project: "visual-studio-code-extensions",
+                url: "https://sentry.debricked.com/",
+            }),
         ],
     });
     if (watch) {
