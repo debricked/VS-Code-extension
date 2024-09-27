c439fa3ed3e228eb99f7d9fc4cfa1d24807384bb
Nagarjun Sanji <nsanji@opentext.com>
2024-09-27 11:40:31 +0530

    updated code

--- src/helpers/debrickedServiceHelper.ts
+++ src/helpers/debrickedServiceHelper.ts
@@ -1,5 +1,5 @@
 import { ApiHelper } from "./apiHelper";
-import { Repository, RequestParam } from "../types/index";
+import { DependencyResponse, DependencyVulnerabilityWrapper, Repository, RequestParam } from "../types/index";
 import { Logger } from "./loggerHelper";
 import { SecondService } from "../constants";
 import { SentryHelper } from "./sentryHelper";
@@ -26,6 +26,36 @@ export class DebrickedServiceHelper {
         try {
             const response = await this.apiHelper.get(requestParam);
             return response.repositories || [];
+        } catch (error: any) {
+            SentryHelper.captureException(error);
+            throw error;
+        }
+    }
+
+    /**
+     * Fetches Dependencies Hierarchy.
+     *
+     * @returns {Promise<DependencyResponse>} A promise resolving to Dependencies Hierarchy.
+     */
+    public async fetchDependenciesHierarchy(requestParam: RequestParam): Promise<DependencyResponse> {
+        this.logger.logInfo("Fetching Dependencies Hierarchy");
+        try {
+            return await this.apiHelper.get(requestParam);
+        } catch (error: any) {
+            SentryHelper.captureException(error);
+            throw error;
+        }
+    }
+
+    /**
+     * Fetches Vulnerabilities.
+     *
+     * @returns {Promise<DependencyVulnerabilityWrapper>} A promise resolving to Vulnerabilities.
+     */
+    public async fetchVulnerabilities(requestParam: RequestParam): Promise<DependencyVulnerabilityWrapper> {
+        this.logger.logInfo("Fetching Vulnerabilities");
+        try {
+            return await this.apiHelper.get(requestParam);
         } catch (error: any) {
             SentryHelper.captureException(error);
             throw error;
